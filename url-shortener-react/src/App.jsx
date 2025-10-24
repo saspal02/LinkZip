59e44d91af4ac6f72bdb87a64b0ceb52c2e6997e
Saswat Pal <saswat2001@gmail.com>
2025-10-25 00:31:25 +0530

    Implement Register Page

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
-import AboutPage from './components/AboutPage' // ✅ Import it
+import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage' 
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/about' element={<AboutPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     <Footer />
     </BrowserRouter>
